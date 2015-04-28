02c7dc5261dc2486e4878ee8dcb967c11c63da48
ivanmayes <ivanmayes@gmail.com>
2015-04-28 16:26:43 -0500

    Fixes for interface, clicking into photos

--- web/public/app/home.js
+++ web/public/app/home.js
@@ -6,14 +6,34 @@ var app = angular.module('PhotoBoothApp',[]),
 	query = require('./queryParams')(document.location.search);
 
 app.controller('main', ['$scope', '$location', '$http', function($scope, $location, $http) {
+    
+	$scope.backButton = backButton;
+    $scope.clickPhoto = clickPhoto;
+    $scope.currentPhotoName;
     $scope.photos = getPhotosJSON();
     $scope.view = 'gallery';
-    $scope.currentPhotoName;
+    $scope.showBackButton = true;
 
     if(query.p) {
     	$scope.view = 'single';
     	$scope.currentPhotoName = query.p;
+    	$scope.showBackButton = false;
     }
+
+    $scope.$on('$locationChangeStart', function(event, route) {
+    	console.log(route);
+    	if(($location.path() == '/' && query.p) || $location.path() == '/single') {
+    		$scope.view = 'single';
+
+    		if(query.p) {
+		    	$scope.currentPhotoName = query.p;
+		    	$scope.showBackButton = false;
+		    }
+
+    	}else{
+    		$scope.view = 'gallery';
+    	}
+    });
 
     function getPhotosJSON() {
     	$http({
@@ -27,6 +47,15 @@ app.controller('main', ['$scope', '$location', '$http', function($scope, $locati
             console.log(error);
             $scope.errors = ['There was an error contacting the server, please try again and contact us if you continue to have issues.'];
         });
+    }
+
+    function clickPhoto(photo) {
+    	$scope.currentPhotoName = photo.name;
+    	$location.path('/single');
+    }
+
+    function backButton() {
+    	$location.path('/');
     }
 
 }]);
