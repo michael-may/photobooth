7db7a547bede6dbe13a389aaff9efa69f957c315
ivanmayes <ivanmayes@gmail.com>
2015-05-14 16:07:11 -0500

    Added pagination, email options, more descriptions for sharing

--- web/public/app/home.js
+++ web/public/app/home.js
@@ -2,6 +2,8 @@
 require('../scss/home.scss');
 require('angular');
 require('../bower_components/ng-simplePagination/simplePagination.js');
+//require('../bower_components/angular-utf8-base64/angular-utf8-base64.js');
+//require('../bower_components/angular-base64/angular-base64.js');
 
 var app = angular.module('PhotoBoothApp',['simplePagination']),
 	query = require('./queryParams')(document.location.search);
@@ -11,18 +13,23 @@ var app = angular.module('PhotoBoothApp',['simplePagination']),
   $locationProvider.html5Mode(true).hashPrefix('!')
 });*/
 
-app.controller('main', ['$scope', '$location', '$http', 'Pagination', function($scope, $location, $http, Pagination) {
+app.controller('main', ['$scope', '$location', '$http', 'Pagination', 
+    function($scope, $location, $http, Pagination) {
 
 	$scope.backButton = backButton;
     $scope.clickPhoto = clickPhoto;
+    $scope.emailFiles = emailFiles;
+    $scope.email = '';
     $scope.currentPhotoName;
     $scope.photos = getPhotosJSON();
     $scope.view = 'gallery';
     $scope.showBackButton = true;
-    $scope.pagination = Pagination.getNew(10);
+    $scope.pagination = Pagination.getNew(9);
     $scope.pagination.numPages = 1;
     $scope.isGifCollapsed = true;
     $scope.isShareCollapsed = true;
+    $scope.isVideoCollapsed = true;
+    $scope.isEmailCollapsed = true;
 
     $scope.$on('$locationChangeStart', function(event, route) {
     	if(query.p) {
@@ -74,5 +81,78 @@ app.controller('main', ['$scope', '$location', '$http', 'Pagination', function($
     function backButton() {
     	$location.path('/');
     }
+
+    function emailFiles(name, email) {
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', 'https://s3.amazonaws.com/shoptology-photo-booth/'+name+'.mp4', true);
+            xhr.responseType = 'arraybuffer';
+             
+            xhr.onload = function(e) {
+              if (this.status == 200) {
+                // get binary data as a response
+                var blob = new Uint8Array(this.response); 
+                console.log('Got Blob', blob);
+
+                // Send Email
+                function Uint8ToString(u8a){
+                  var CHUNK_SZ = 0x8000;
+                  var c = [];
+                  for (var i=0; i < u8a.length; i+=CHUNK_SZ) {
+                    c.push(String.fromCharCode.apply(null, u8a.subarray(i, i+CHUNK_SZ)));
+                  }
+                  return c.join("");
+                }
+                var file = btoa(Uint8ToString(blob));
+                //var file = base64.encode(blob);
+
+                console.log('Sending email');
+
+                $http.post(
+                    'https://mandrillapp.com/api/1.0/messages/send.json',
+                    {
+                        key: 'eFx6Arc4lLspc5HmPmwvxQ',
+                        'message': {
+                            'from_email': 'no-reply@goshoptology.com',
+                            'from_name': 'Shoptology',
+                            'headers': {
+                                'Reply-To': 'no-reply@goshoptology.com'
+                            },
+                            'subject': 'Here are your gifs from Block Street Party',
+                            'important': true,
+                            'html': '<html>Here are your images from Block Street Party, thanks for visiting! - The Shoptology Team<\/html>',
+                            'to': [
+                                {
+                                    'email': email,
+                                    'type': 'to'
+                                }
+                            ],
+                            'attachments': [
+                                {
+                                    'type': 'video/mp4',
+                                    'name': $scope.currentPhotoName+'.mp4',
+                                    'content': file
+                                }
+                            ]
+                        }
+                    }
+                )
+                .then(function(result) {
+                    console.log('email sent')
+                    
+                }, function(error) {
+                    console.log('error', error);
+                    $scope.errors = ['There was an error contacting the server, please try again and contact us if you continue to have issues.'];
+                });
+
+              }else{
+                console.log('error', error);
+                $scope.errors = ['There was an error contacting the server, please try again and contact us if you continue to have issues.'];
+              }
+            };
+             
+            xhr.send();
+    }
+
 
 }]);
